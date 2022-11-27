3a8d7793b9106540b0ca0c15a745e8e06c962f22
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2022-11-27 22:49:46 +0900

    Home.js 추가, home.css 변경해줄 필요있음.

--- Home.js
+++ Home.js
@@ -2,19 +2,13 @@ const $address = 'http://ec2-43-201-47-225.ap-northeast-2.compute.amazonaws.com'
 
 //윈도우 실행시
 window.onload=function(){
-// storage에 저장된 accessToken과 username 받아오기  
+// storage에 저장된 accessToken과 email 받아오기  
   accessToken = localStorage.getItem('accessToken');
-  username = localStorage.getItem('username');
   email = localStorage.getItem('email');
 
-  // 사용자이름 출력
-  const element = document.getElementById('username');
-  const newSpan = document.createTextNode('어서오세요! '+username+'님!');
-  element.appendChild(newSpan);
   // 화면 정보 받아들이고 만들기
   load_project_data();
 }
-
 // 프로젝트 데이터 가져오기
 function load_project_data(){
   // json 형식으로 가져와서 저장
@@ -28,12 +22,11 @@ function load_project_data(){
         // console.log(xhr.responseText);
         //변환하여 저장
         data = JSON.parse(xhr.responseText);
-        console.log(data[1].id);
         }
         else {
         // 오류시 localStorage를 초기화하고 로그인화면으로
         localStorage.clear();
-        window.open('./Login.html');
+        location.href='./Login.html';
       }
     }
   }
@@ -49,13 +42,9 @@ function load_project_data(){
     element111.append(noprojectdiv);
     } else {
       for(let i=0; i<data.length; i++){
-        // 화면만들기~~
+      // json 형식으로 가져온걸로 프로젝트 화면만들기~~
         makediv(data[i].id, data[i].title, data[i].description)
       }
-      // json 형식으로 가져온걸로 프로젝트 화면 만들기
-      console.log("there is data");
-      console.log("배열 갯수"+data.length);
-      console.log(data[1].id); 
     }
   }
 }
@@ -64,42 +53,69 @@ function load_project_data(){
 function makediv(projectid,title,description){
   const element = document.getElementById('projects');
   
-  const newdiv = document.createElement('div')
+  const newdiv = document.createElement('div');
+  const newdeletediv = document.createElement('div');
 
   // newdiv에 스타일 설정
   newdiv.classList.add('newdiv');
+  // newdiv에 객체 id 설정
+  newdiv.id = projectid;
 
-  const newprojectid_div = document.createElement('div');
+  //삭제하는 div에 스타일추가
+  newdeletediv.classList.add('deletediv')
+  
+  const deletediv_text = document.createTextNode("delete project")
+  newdeletediv.append(deletediv_text)
+
+  // 프로젝트 누를시 이동하는함수
+  newdiv.onclick = function(){ 
+    //클릭될시 projectid를 로컬 스토리지에project id로 저장
+     localStorage.setItem('projectid',projectid);
+    // 페이지 이동
+    location.href="./Task/DnD.html";
+  }
+  
+  // 프로젝트 삭제하는 oncllick 함수
+  newdeletediv.onclick = function(){
+    projectdelete(projectid);
+  }
+
+
   const newtitle_div = document.createElement('div');
-  const newdescription_div = document.createElement('div');
+  newtitle_div.classList.add('title')
 
-  const newprojectid = document.createTextNode("프로젝트 id"+""+projectid);
+  const newdescription_div = document.createElement('div');
+  newdescription_div.classList.add('describe')
+
   const newtitle = document.createTextNode("프로젝트 타이틀"+title);
   const newdescription = document.createTextNode("프로젝트 설명"+description);
 
-  newprojectid_div.append(newprojectid);
   newtitle_div.append(newtitle);
   newdescription_div.append(newdescription);
 
-  newdiv.append(newprojectid_div,newtitle_div,newdescription_div);
+  newdiv.append(newtitle_div,newdescription_div,newdeletediv);
 
   element.append(newdiv);
-
 }
 
-// 프로젝트 추가를 눌렀을때
-function addproject(){
-  window.open('./ProjectRegister.html');
-}
-
-// 프로젝트를 눌렀을때
-function project_click(){
-  // 프로젝트 id local storage에 저장
-
-  // 페이지 이동
-}
 
 // 프로젝트 삭제
-function projectdelete(){
-  
+function projectdelete(projectid){
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE", $address+'/project/'+projectid, true); 
+  xhr.setRequestHeader('Authorization',"Bearer " + accessToken);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        //성공시 페이지 refresh
+        location.href='./Home.html';
+        }
+        else {
+        // 오류시 localStorage를 초기화하고 로그인화면으로
+        localStorage.clear();
+        location.href='./Login.html';
+      }
+    }
+  }
+  xhr.send();
 }
